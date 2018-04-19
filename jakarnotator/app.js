6e0de6e4e87a3aba9e0ed80970a24fe028a380ee
Loïc Messal <loic.messal@jakarto.com>
2018-04-18 20:21:54 -0400

    change application title

--- jakarnotator/app.js
+++ jakarnotator/app.js
@@ -12,6 +12,24 @@ var processRouter = require("./routes/process");
 
 var app = express();
 
+const Prometheus = require('prom-client')
+const metricsInterval = Prometheus.collectDefaultMetrics()
+const checkoutsTotal = new Prometheus.Counter({
+  name: 'checkouts_total',
+  help: 'Total number of checkouts',
+  labelNames: ['payment_method']
+})
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]  // buckets for response time from 0.1ms to 500ms
+})
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(Prometheus.register.metrics())
+})
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
