61e283b6354eab5d5013748ae11118fa1af85c8a
Loïc Messal <loic.messal@jakarto.com>
2018-04-30 17:09:28 -0400

    clean file

--- jakarnotator/app.js
+++ jakarnotator/app.js
@@ -3,28 +3,33 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const promBundle = require("express-prom-bundle");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var masksRouter = require("./routes/masks");
 var imagesRouter = require("./routes/images");
 var processRouter = require("./routes/process");
-const promBundle = require("express-prom-bundle");
-var app = express();
-const metricsMiddleware = promBundle({ includeMethod: true });
 
-app.use(metricsMiddleware);
+var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// metrics collection setup
+const metricsMiddleware = promBundle({ includeMethod: true });
+app.use(metricsMiddleware);
+
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// routes setup
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/scripts", express.static(path.join(__dirname, "node_modules/vue2-bootstrap-table2/dist")));
+app.use("/vendors", express.static(path.join(__dirname, "node_modules/vue2-bootstrap-table2/dist")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
