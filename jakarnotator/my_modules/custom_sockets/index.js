7639ceb99a591d2fb0412108bf293cef3d1a4fed
Loïc Messal <loic.messal@jakarto.com>
2018-05-06 01:25:19 -0400

    remove geosocket scories

--- jakarnotator/my_modules/custom_sockets/index.js
+++ jakarnotator/my_modules/custom_sockets/index.js
@@ -2,7 +2,7 @@
 
 var socket_io = require("socket.io"); // Utilities for real time connection
 
-function GEOSOCKET(server) {
+function Sockets(server) {
     var io = socket_io();
 
     // Gestion des sockets
@@ -10,19 +10,6 @@ function GEOSOCKET(server) {
 
         console.log(socket.id + " is now connected ! ");
 
-        // // Envoi à tous les clients qu'un nouveau client vient de se connecter au serveur
-        // socket.broadcast.emit('new_client', { socketid: socket.id });
-
-        // // Envoi à tous les clients du nombre de clients connectés
-        // io.sockets.emit('count', { nombreClients: io.engine.clientsCount });
-
-        // // Envoi au nouveau client les informations des clients connectés avant sa connexion
-        // socket.on('present_to_new_client', function (data) {
-        //     console.log(socket.id + " se presente à " + data.socketid);
-        //     io.sockets.connected[data.socketid].emit('coordonneesBroadcast', { socketid: socket.id, pos: data.pos, name: data.name });
-        // });
-        
-        // once a client has connected, we expect to get a ping from them saying what room they want to join
         socket.on('room-join', function (room) {
             socket.join(room);
             console.log(socket.id + " has joined the room  " + room)
@@ -36,41 +23,15 @@ function GEOSOCKET(server) {
         socket.on('send_a_new_json', function (room) {
             console.log("To all connected people in the room " + room + ". You should refresh the json for this image.")
             socket.to(room).broadcast.emit('should_refresh_json');
-            // io.sockets.connected[data.socketid].emit('coordonneesBroadcast', { socketid: socket.id, pos: data.pos, name: data.name });
         });
 
-        // // Diffusion d'un changement de position à tous les clients
-        // socket.on('coordonneesClient', function (data) {
-        //     socket.broadcast.emit('coordonneesBroadcast', { socketid: socket.id, pos: data.pos, name: data.name });
-        // });
-
-        // // Diffusion d'un changement de position à tous les clients
-        // socket.on('warningRisk', function (data) {
-        //     console.log("Risk received.")
-        //     socket.broadcast.emit('warningRiskMessage', { message: data.message });
-        // });
-
-        // // Lors d'une deconnexion
-        // socket.once('disconnect', function () {
-        //     // Envoi à tous les clients du nombre de clients connectés
-        //     socket.broadcast.emit('count', {
-        //         nombreClients: io.engine.clientsCount
-        //     });
-        //     // Envoi de l'identifiant du client qui vient de se deconnecter pour l'effacer de la carte
-        //     socket.broadcast.emit('clientLeft', { socketid: socket.id });
-        // });
         socket.on('disconnect', function () {
             console.log(socket.id + 'user disconnected');
         });
     });
 
     io.attach(server);
-
-    // return function GEOSOCKET(req, res, next) {
-    //     res.io = io;
-    //     return next();
-    // };
 }
 
 
-module.exports = GEOSOCKET;
+module.exports = Sockets;
