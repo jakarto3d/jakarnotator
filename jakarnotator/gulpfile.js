fa89c163cb760e8ad85b6878365e9e912330a301
Loïc Messal <loic.messal@jakarto.com>
2018-05-07 15:00:52 -0400

    es6-like syntax

--- jakarnotator/gulpfile.js
+++ jakarnotator/gulpfile.js
@@ -1,31 +1,31 @@
 'use strict';
 
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var nodemon = require('gulp-nodemon');
+let gulp = require('gulp');
+let browserSync = require('browser-sync');
+let nodemon = require('gulp-nodemon');
 
-var EXPOSED_PORT = process.env.PORT || 8080
-var SYNC_PORT = process.env.SYNCPORT || 8081
+let EXPOSED_PORT = process.env.PORT || 8080;
+let SYNC_PORT = process.env.SYNCPORT || 8081;
 
-gulp.task('default', ['browser-sync'], function () {});
+gulp.task('default', ['browser-sync'], function() {});
 
-gulp.task('browser-sync', ['nodemon'], function () {
+gulp.task('browser-sync', ['nodemon'], function() {
     browserSync.init(null, {
         proxy: `http://localhost:${EXPOSED_PORT}`,
-        files: ["public/(?!data)**/*.*", "views/**/*.*", "routes/**/*.*"],
-        ignore: ["public/data/**.*"],
+        files: ['public/(?!data)**/*.*', 'views/**/*.*', 'routes/**/*.*'],
+        ignore: ['public/data/**.*'],
         port: SYNC_PORT,
     });
 });
 
-gulp.task('nodemon', function (cb) {
-    var started = false;
+gulp.task('nodemon', function(cb) {
+    let started = false;
     return nodemon({
         script: './bin/www',
         env: {
-            PORT: EXPOSED_PORT
+            PORT: EXPOSED_PORT,
         },
-    }).on('start', function () {
+    }).on('start', function() {
         // to avoid nodemon being started multiple times
         // thanks @matthisk
         if (!started) {
